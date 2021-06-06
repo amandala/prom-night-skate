e62a4e35d8a6ee939b5d10b957ff8f53c209072d
Amanda Haynes <dev@portad.ca>
2021-06-05 19:13:34 -0600

    home back

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "../components/Head";
+import Header from "../components/Header";
+import Gallery from "../components/Gallery";
+import FAQ from "../components/FAQ";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <Header />
+        <Gallery />
+        <FAQ />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
